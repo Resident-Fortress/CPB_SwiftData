14eee0fa97a86308c97a8ee24c86cbe885aa81ca
Matthew Dudzinski <42253757+Resident-Fortress@users.noreply.github.com>
2024-09-26 10:12:40 -0500

    Initial Commit

---
+++ CPB_SwiftData/CPB_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPB_SwiftDataApp.swift
+//  CPB_SwiftData
+//
+//  Created by Matthew Dudzinski on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CPB_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
